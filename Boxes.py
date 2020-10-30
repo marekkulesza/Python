71f9b3e3ca68c94d082b906b1b9e6fab7b0aa2aa
marekkulesza <66383506+marekkulesza@users.noreply.github.com>
2020-10-30 11:46:28 -0400

    Update Boxes.py

--- Boxes.py
+++ Boxes.py
@@ -1,13 +1,15 @@
-size = [5,6,7,8,9,10]
+# Testing multiple box sizes instead of just making one
+size = list(range(5,20))
 column = size
 row = size
 
-
+# This for loop checks out the numbers in the size list and sets them to the size
 for element in size:
     size = element
     column = element
     row = element
 
+    # This creates the boxes
     while row > 0:
         while column > 0:
             if column == element:
